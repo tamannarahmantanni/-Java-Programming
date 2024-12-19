4ec359c37e9c17d6628cffc951d276b7c834dd63
Tamanna <tamannajnutanni@gmail.com>
2024-12-19 20:37:32 +0600

    first commit

--- src/main/java/N12_DiscountCalculator.java
+++ src/main/java/N12_DiscountCalculator.java
@@ -1,23 +1,21 @@
 public class N12_DiscountCalculator {
     public static void main(String[] args) {
-        String paragraph = "A core i 7 laptop price is 85000 tk and a gaming mouse price is 2500 tk. If I buy the laptop and 1 piece mouse, what will be my total cost after giving 15% discount?";
-        String[] words = paragraph.split(" ");
-        int laptop_Price = 0;
-        int mouse_Price = 0;
-        for (String word : words) {
-            if (word.matches("\\d+")) {
-                int price = Integer.parseInt(word);
-                if (laptop_Price == 0) {
-                    laptop_Price = price;
-                } else {
-                    mouse_Price = price;
-                    break;
-                }
-            }
-        }
-        int totalCost = laptop_Price + mouse_Price;
-        double discount_Rate = 0.15;
-        double AfterDiscount = totalCost * (1 - discount_Rate);
-        System.out.printf("Total cost after 15%% discount: %.2f tk%n", AfterDiscount);
+        String description = "A core i 7 laptop price is 85000 tk and a gaming mouse price is 2500 tk. If I buy the laptop and 1 piece mouse, what will be my total cost after giving 15% discount?";
+
+        String numeric_Data = description.replaceAll("[^\\d]", " ");
+        numeric_Data = numeric_Data.trim();
+        numeric_Data = numeric_Data.replaceAll(" +", " ");
+        String[] numbersArray = numeric_Data.split(" ");
+
+        int laptop_Cost = Integer.parseInt(numbersArray[1]);
+        int mouse_Cost = Integer.parseInt(numbersArray[2]);
+        int mouse_Count = Integer.parseInt(numbersArray[3]);
+        int discount_Rate = Integer.parseInt(numbersArray[4]);
+
+        int totalCostBeforeDiscount = (laptop_Cost + (mouse_Cost * mouse_Count));
+        Double discount_Amount = totalCostBeforeDiscount * (discount_Rate / 100.0);
+        double finalCost = totalCostBeforeDiscount - discount_Amount;
+
+        System.out.println("Total cost after 15%  discount: " + finalCost );
     }
 }
